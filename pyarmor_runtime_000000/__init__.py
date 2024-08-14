083648a572bd0806cefbcd5135c1fd640c8be09a
Ali <vynz420@gmail.com>
2024-08-14 20:13:23 +0500

    1.0.0.L_A

--- pyarmor_runtime_000000/__init__.py
+++ pyarmor_runtime_000000/__init__.py
@@ -1,2 +1,2 @@
-# Pyarmor 8.5.10 (trial), 000000, 2024-08-14T18:43:08.232792
+# Pyarmor 8.5.10 (trial), 000000, 2024-08-14T20:11:39.641866
 from .pyarmor_runtime import __pyarmor__
--- run.bat
+++
@@ -1,6 +0,0 @@
-:Start 
-TASKKILL /IM chrome.exe /F
-python DialerRun.py 
-:: wait 30
-TIMEOUT /T 30
-GOTO:Start
